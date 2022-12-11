b391f9712f8f1df6fb6bb448bc389f44cd33d8a7
konovod <konovod@gmail.com>
2022-12-11 11:02:35 +0300

    template dammit

---
+++ src/days/day_template.rs
@@ -0,0 +1,11 @@
+pub fn parse(input: String) -> Vec<String> {
+    return input.split("\n").map(|x| x.trim().to_string()).collect();
+}
+
+pub fn part1(input: String) -> i32 {
+    return 0;
+}
+
+pub fn part2(input: String) -> i32 {
+    return 0;
+}
---
+++ tests/day_template.rs
@@ -0,0 +1,11 @@
+use aoc;
+
+#[test]
+fn test1() {
+    assert_eq!(aoc::days::day_template::part1("123".to_string()), 0);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(aoc::days::day_template::part2("123".to_string()), 0);
+}
